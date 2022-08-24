de2a9d40172704f36420191015f60186e796ed04
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-08-24 20:50:52 +0500

    Задача 13

---
+++ DZ_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+
+string str = num.ToString();
+int minLength = 2;
+
+if(str.Length > minLength)
+{
+    Console.WriteLine($"{num} -> {str[2]}");
+}
+else
+    Console.WriteLine(num + " -> третьей цифры нет.");
+
